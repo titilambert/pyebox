155e92ff255627bd390b34cf149b0466e947b41f
Thibault Cohen <titilambert@gmail.com>
2017-11-21 20:03:47 -0500

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -7,17 +7,25 @@ install_reqs = parse_requirements('requirements.txt', session=session)
 test_reqs = parse_requirements('test_requirements.txt', session=session)
 
 setup(name='pyebox',
-      version='0.1.0',
+      version='1.0.0',
       description='Get your EBox consumption (wwww.ebox.ca)',
       author='Thibault Cohen',
       author_email='titilambert@gmail.com',
       url='http://github.org/titilambert/pyebox',
+      package_data={'': ['LICENSE.txt']},
+      include_package_data=True,
       packages=['pyebox'],
       entry_points={
           'console_scripts': [
               'pyebox = pyebox.__main__:main'
           ]
       },
+      license='Apache 2.0',
       install_requires=[str(r.req) for r in install_reqs],
       tests_require=[str(r.req) for r in test_reqs],
+      classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ],
 )
