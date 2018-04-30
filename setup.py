770a45eb03129dd664d565490a206063050b550b
Thibault Cohen <thibault_cohen@manulife.com>
2018-04-30 13:24:22 -0400

    Release 1.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = list(val.strip() for val in open('requirements.txt'))
 tests_require = list(val.strip() for val in open('test_requirements.txt'))
 
 setup(name='pyebox',
-      version='1.1.2',
+      version='1.1.3',
       description='Get your EBox consumption (wwww.ebox.ca)',
       author='Thibault Cohen',
       author_email='titilambert@gmail.com',
